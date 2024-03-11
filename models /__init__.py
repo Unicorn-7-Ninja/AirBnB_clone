6caadea217cc639d04228b5324116b015009cb23
Unicorn-7-Ninja <122676935+Unicorn-7-Ninja@users.noreply.github.com>
2024-03-11 00:40:28 +0000

    Create  __init__.py

--- models /__init__.py
+++ models /__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""__init__ magic method for models directory"""
+""" the __init__ magic method for models directory"""
 from .base_model import BaseModel
 
 from models.engine.file_storage import FileStorage
