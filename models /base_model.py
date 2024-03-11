a16ee13bf4d8dc6df1fa5ca97d612427632a89da
Unicorn-7-Ninja <122676935+Unicorn-7-Ninja@users.noreply.github.com>
2024-03-11 00:41:51 +0000

    Create base_model.py

--- models /base_model.py
+++ models /base_model.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""BaseModel class """
+"""BaseModel class"""
 import models
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """BaseModel of the HBnB project."""
+    """BaseModel of the HBnB project"""
 
     def __init__(self, *args, **kwargs):
-        """Initialize the BaseModel.
+        """Initialize the BaseModel & 
 
         Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
+            *args (any): Unused
+            **kwargs (dict): Key/value pairs of attributes
         """
         tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
@@ -29,15 +29,15 @@ class BaseModel:
             models.storage.new(self)
 
     def save(self):
-        """Update updated_at with the current datetime."""
+        """Update updated_at with the current datetime"""
         self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
+        """Return the dictionary of the BaseModel instance
 
         Includes the key/value pair __class__ representing
-        the class name of the object.
+        the class name of the object
         """
         rdict = self.__dict__.copy()
         rdict["created_at"] = self.created_at.isoformat()
@@ -46,6 +46,6 @@ class BaseModel:
         return rdict
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
+        """Return the print/str representation of the BaseModel instance"""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
