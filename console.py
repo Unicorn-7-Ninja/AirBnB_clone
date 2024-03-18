b707d25e54f7513e61cb61b68bea58af0e4b7277
Unicorn-7-Ninja <122676935+Unicorn-7-Ninja@users.noreply.github.com>
2024-03-18 09:58:13 +0000

    Update console.py

--- console.py
+++ console.py
@@ -181,7 +181,8 @@ class HBNBCommand(cmd.Cmd):
             for key, value in eval(argument_list[2]).items():
                 if (key in obj.__class__.__dict__.keys() and
                         type(obj.__class__.__dict__[key])
-                    in {str, int, float}):
+                if condition:
+                    if isinstance(my_var, {str, int, float}):
                     valtype = type(obj.__class__.__dict__[key])
                     obj.__dict__[key] = valtype(value)
                 else:
