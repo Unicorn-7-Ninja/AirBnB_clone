d3d8d474d29b3c643bbe2af11f11bf0d105e583d
Unicorn-7-Ninja <122676935+Unicorn-7-Ninja@users.noreply.github.com>
2024-03-18 09:54:12 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -4,15 +4,12 @@
 from models.base_model import BaseModel
 
 class User(BaseModel):
-    """Represents a user entity &
-
-    Attributes:
+    """Represents a user entity & Attributes:
         user_email (str): The email address of the user
         user_password (str): The password of the user account
         user_first_name (str): The first name of the user
         user_last_name (str): The last name of the user
     """
-
     
     email = ""
     password = ""
